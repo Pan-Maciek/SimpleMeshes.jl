d520ed665b038d01e257692a04e7bc0b46371743
Pan-Maciek <koziejka.com@gmail.com>
2020-11-14 16:22:45 +0100

    fixed includes

--- src/SimpleMeshes.jl
+++ src/SimpleMeshes.jl
@@ -3,14 +3,22 @@ module SimpleMeshes
 using OffsetArrays
 using LinearAlgebra
 
-include("utils.jl")
 include("points.jl")
 include("faces.jl")
-include("meshes.jl")
+
+include("meshes/base.jl")
+include("meshes/mesh.jl")
+include("meshes/halfedge.jl")
+
+include("utils.jl")
+
+include("io/files.jl")
+include("io/ply.jl")
 
 export AbstractPoint, Point, Point2, Point2f, Point3, Point3f,
   AbstractPolygon, Polygon, Triangle,
-  AbstractMesh, Mesh, TriangleMesh, 
-  faceswith, vneighbors, fneighbors, traverse, flipdiag!
+  AbstractMesh, Mesh, TriangleMesh, HalfEdgeMesh,
+  face, vertex, edges,
+  File, FileFormat, format_str, add_format, load
 
 end
