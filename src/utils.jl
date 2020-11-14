17699db20c8a7dde59f60256ff5e0e1c0a6afd28
Pan-Maciek <koziejka.com@gmail.com>
2020-11-14 02:15:25 +0100

    utils

--- src/utils.jl
+++ src/utils.jl
@@ -1,1 +1,17 @@
 zerobased(array::Vector) = OffsetVector(array, 0:length(array)-1)
+midpoint(A, B) = (A .+ B) ./ 2.
+
+function facenorm(mesh::Mesh, faceidx)
+  @inbounds A, B, C = vertex(mesh, [face(mesh, faceidx)[1:3]...])
+  normalize((B .- A) × (C .- A))
+end
+
+function facenorm(mesh::HalfEdgeMesh, faceidx)
+  h = mesh.halfedges[mesh.faces[faceidx]]
+  @inbounds A, B, C = vertex(mesh, [
+    mesh.halfedges[h.prev].origin,
+    h.origin,
+    mesh.halfedges[h.next].origin
+  ])
+  normalize((B .- A) × (C .- A))
+end
